11a5fa0c3684ba4834c78aecd555c4b5046d7ada
Jack Kuang <kuangzijian1@hotmail.com>
2020-01-03 17:54:45 -0700

    Added face swap implementation

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as plt
+import os
+import cv2
+import argparse
+
+import numpy as np
+
+from face_detection import face_detection
+from face_points_detection import face_points_detection
+from face_swap import warp_image_2d, warp_image_3d, mask_from_points, apply_mask, correct_colours, transformation_from_points
 from generate_synthetic_face import generate_synthetic_face
 
 
