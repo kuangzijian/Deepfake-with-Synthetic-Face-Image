2e3ed8b215c97680e16b7dc9250e9d40c5d6e904
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 07:54:19 +0000

    Adding tarfile member sanitization to extractall()

--- src/ingestion/process_transient_attribute_scenes.py
+++ src/ingestion/process_transient_attribute_scenes.py
@@ -29,9 +29,47 @@ if not os.path.exists(path_file_datalabel):
 ##
 # untar datafile
 with tarfile.open(path_file_dataset) as f:
-    f.extractall(path_data)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(f, path_data)
 
 with tarfile.open(path_file_datalabel) as f:
-    f.extractall(path_data)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(f, path_data)
 
 
